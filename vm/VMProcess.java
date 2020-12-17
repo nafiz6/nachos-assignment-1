1280cea21671b16e15e284aaf48ebd52abfbe082
Fizz <nafiz6@yahoo.com>
2020-12-17 23:20:38 +0600

    table

--- vm/VMProcess.java
+++ vm/VMProcess.java
@@ -5,69 +5,90 @@ import nachos.threads.*;
 import nachos.userprog.*;
 import nachos.vm.*;
 
+import java.util.Hashtable;
+
 /**
  * A <tt>UserProcess</tt> that supports demand-paging.
  */
 public class VMProcess extends UserProcess {
-    /**
-     * Allocate a new process.
-     */
-    public VMProcess() {
-	super();
-    }
-
-    /**
-     * Save the state of this process in preparation for a context switch.
-     * Called by <tt>UThread.saveState()</tt>.
-     */
-    public void saveState() {
-	super.saveState();
-    }
-
-    /**
-     * Restore the state of this process after a context switch. Called by
-     * <tt>UThread.restoreState()</tt>.
-     */
-    public void restoreState() {
-	super.restoreState();
-    }
-
-    /**
-     * Initializes page tables for this process so that the executable can be
-     * demand-paged.
-     *
-     * @return	<tt>true</tt> if successful.
-     */
-    protected boolean loadSections() {
-	return super.loadSections();
-    }
-
-    /**
-     * Release any resources allocated by <tt>loadSections()</tt>.
-     */
-    protected void unloadSections() {
-	super.unloadSections();
-    }    
-
-    /**
-     * Handle a user exception. Called by
-     * <tt>UserKernel.exceptionHandler()</tt>. The
-     * <i>cause</i> argument identifies which exception occurred; see the
-     * <tt>Processor.exceptionZZZ</tt> constants.
-     *
-     * @param	cause	the user exception that occurred.
-     */
-    public void handleException(int cause) {
-	Processor processor = Machine.processor();
-
-	switch (cause) {
-	default:
-	    super.handleException(cause);
-	    break;
+	/**
+	 * Allocate a new process.
+	 */
+	public VMProcess() {
+		super();
 	}
-    }
-	
-    private static final int pageSize = Processor.pageSize;
-    private static final char dbgProcess = 'a';
-    private static final char dbgVM = 'v';
+
+	/**
+	 * Save the state of this process in preparation for a context switch.
+	 * Called by <tt>UThread.saveState()</tt>.
+	 */
+	public void saveState() {
+		super.saveState();
+	}
+
+	/**
+	 * Restore the state of this process after a context switch. Called by
+	 * <tt>UThread.restoreState()</tt>.
+	 */
+	public void restoreState() {
+		super.restoreState();
+	}
+
+	/**
+	 * Initializes page tables for this process so that the executable can be
+	 * demand-paged.
+	 *
+	 * @return	<tt>true</tt> if successful.
+	 */
+	protected boolean loadSections() {
+		return super.loadSections();
+	}
+
+	/**
+	 * Release any resources allocated by <tt>loadSections()</tt>.
+	 */
+	protected void unloadSections() {
+		super.unloadSections();
+	}    
+
+	/**
+	 * Handle a user exception. Called by
+	 * <tt>UserKernel.exceptionHandler()</tt>. The
+	 * <i>cause</i> argument identifies which exception occurred; see the
+	 * <tt>Processor.exceptionZZZ</tt> constants.
+	 *
+	 * @param	cause	the user exception that occurred.
+	 */
+	public void handleException(int cause) {
+		Processor processor = Machine.processor();
+
+		switch (cause) {
+			default:
+				super.handleException(cause);
+				break;
+		}
+	}
+
+	private static final int pageSize = Processor.pageSize;
+	private static final char dbgProcess = 'a';
+	private static final char dbgVM = 'v';
+	private static Hashtable<Pair, Integer> invertedPageTable = new Hashtable<>();
+
+	class Pair{
+		int pid;int vpn;	
+		Pair(int p, int v) {
+			pid = p;vpn = v;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			Pair temp = (Pair) obj;
+			return temp.pid == pid && temp.vpn == vpn;
+		}
+
+		@Override
+		public int hashCode() {
+			return (pid * 153 + vpn * 177) % Integer.MAX_VALUE;
+		}
+	}
 }
